856b2979f071b941063561b7ccd10b6ca6e48cbe
Pan Deng <pan.deng@intel.com>
2025-04-16 17:31:57 +0800

    Fix proxy error

--- app/scripts/launch_cosyvoice_test.sh
+++ app/scripts/launch_cosyvoice_test.sh
@@ -6,6 +6,6 @@ ln -s /usr/bin/python3 /usr/bin/python
 cd ~/open-avatar-chat/src/handlers/tts/cosyvoice/CosyVoice/examples/libritts/cosyvoice
 
 export http_proxy=http://proxy-prc.intel.com:913
-export https_proxy=https://proxy-prc.intel.com:913
+export https_proxy=http://proxy-prc.intel.com:913
 
 bash run.sh
